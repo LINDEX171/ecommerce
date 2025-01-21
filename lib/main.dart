8a0dc7cb649bf73a1a05748931f7e81a9e3772c9
LINDEX171 <lindex171@gmail.com>
2025-01-21 13:54:20 +0000

    theme.dart,appbartheme,bottomsheettheme,elevatedbuttontheme,texttheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,23 @@
+import 'package:ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTeme,
+      darkTheme: TAppTheme.darkTheme,
+      home: Scaffold(
+        appBar: AppBar(),
       ),
-      home: HomePage(),
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blue),
-      body: Text("HI world",style: TextStyle(fontFamily: "Poppins")),
-    );
-  }
-}
-
