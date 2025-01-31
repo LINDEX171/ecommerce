8bb2e89651d0cd078d2bb1830636ea5c16618109
LINDEX171 <lindex171@gmail.com>
2025-01-31 16:13:43 +0000

    dot navigation and nextbutton for onboarding

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,9 +1,15 @@
+import 'package:ecommerce/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:ecommerce/features/authentication/screens/onboarding/widgets/onboardint_dot_navigation.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:ecommerce/utils/constants/image_strings.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:ecommerce/utils/constants/text_strings.dart';
 import 'package:ecommerce/utils/device/device_utility.dart';
 import 'package:ecommerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import 'widgets/onboarding_page.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -16,66 +22,56 @@ class OnBoardingScreen extends StatelessWidget {
           ///horizontal Scrollable page
           PageView(
             children: const [
-              OnBoardingPage(image: TImages.onBoardingImage1,title: TTexts.onBoardingTitle1,subtitle: TTexts.onBoardingSubTitle1,),
-              OnBoardingPage(image: TImages.onBoardingImage2,title: TTexts.onBoardingTitle2,subtitle: TTexts.onBoardingSubTitle2,),
-              OnBoardingPage(image: TImages.onBoardingImage3,title: TTexts.onBoardingTitle3,subtitle: TTexts.onBoardingSubTitle3,),
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subtitle: TTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subtitle: TTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subtitle: TTexts.onBoardingSubTitle3,
+              ),
             ],
           ),
-          OnBoardingSkip()
 
+          /// Skip Button
+          OnBoardingSkip(),
+
+          ///dot navigation smoothpageindicator
+          OnBoardingDotNavigation(),
+
+          /// Circular button
+          OnBoardingNextButton()
         ],
       ),
     );
   }
 }
 
-class OnBoardingSkip extends StatelessWidget {
-  const OnBoardingSkip({
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(top: TDeviceUtils.getAppBarHeight(),right:TSizes.defaultSpace,
-      child: TextButton(onPressed: () {
-
-      }, child: const Text("Skip")),
-    );
-  }
-}
-
-class OnBoardingPage extends StatelessWidget {
-  const OnBoardingPage({
-    super.key,
-      required this.image, required this.title, required this.subtitle,
-  });
-
- final String image,title,subtitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(TSizes.defaultSpace),
-      child: Column(
-        children: [
-          Image(
-            width: THelperFunctions.screenWidth() * 0.8,
-            height: THelperFunctions.screenHeight() * 0.6,
-            image: AssetImage(image),
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      right: TSizes.defaultSpace,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: CircleBorder(),
+            backgroundColor: dark ? TColors.primary : Colors.black,
           ),
-          Text(
-            title,
-            style: Theme.of(context).textTheme.headlineMedium,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height:TSizes.spaceBtwItems,),
-          Text(
-            subtitle,
-            style: Theme.of(context).textTheme.bodyMedium,
-            textAlign: TextAlign.center,
-          )
-        ],
-      ),
+          onPressed: () {},
+          child: Icon(Icons.arrow_forward_ios_rounded)),
     );
   }
 }
---
+++ lib/features/authentication/screens/onboarding/widgets/onboardint_dot_navigation.dart
@@ -0,0 +1,25 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/device/device_utility.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+
+class OnBoardingDotNavigation extends StatelessWidget {
+  const OnBoardingDotNavigation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+        bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+        left: TSizes.defaultSpace,
+        child:
+        SmoothPageIndicator(
+            effect: ExpandingDotsEffect(activeDotColor: dark ? TColors.white :TColors.dark, dotHeight: 6),
+            controller: PageController(), count: 3));
+  }
+}
