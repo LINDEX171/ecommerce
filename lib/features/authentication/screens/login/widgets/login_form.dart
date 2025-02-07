3f7786f637d2b2bacdfaa85e0ede6722b5e13298
LINDEX171 <lindex171@gmail.com>
2025-02-07 16:08:38 +0000

    reset passwordscreen

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:ecommerce/features/authentication/screens/signup/signup.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:ecommerce/utils/constants/text_strings.dart';
@@ -52,7 +53,7 @@ class TLoginForm extends StatelessWidget {
                 ),
 
                 ///forget Password
-                TextButton(onPressed: () {}, child: const Text(TTexts.forgetPassword),),
+                TextButton(onPressed: () => Get.to(() =>const ForgetPassword()), child: const Text(TTexts.forgetPassword),),
               ],
             ),
             const SizedBox(height: TSizes.spaceBtwSections,),
