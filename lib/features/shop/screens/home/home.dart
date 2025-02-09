747427f660747758e7009881ced1ec66f720865f
LINDEX171 <lindex171@gmail.com>
2025-02-09 16:30:00 +0000

    circular container reusable

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,7 @@ class HomeScreen extends StatelessWidget {
               child: Stack(
                 children: [
                   TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1)),
+                  TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1)),
                 ],
               ),
             )
@@ -27,30 +29,4 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class TCircularContainer extends StatelessWidget {
-  const TCircularContainer({
-    super.key, this.width=400, this.height=400,  this.radius=400,this.padding=400, this.child,this.backgroundColor=TColors.white,
-  });
 
-  final double? width;
-  final double? height;
-  final double radius;
-  final double padding;
-  final Widget? child;
-  final Color backgroundColor;
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width,
-      padding:  EdgeInsets.all(padding),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius),
-        color: backgroundColor,
-      ),
-      child: child,
-    );
-  }
-}
