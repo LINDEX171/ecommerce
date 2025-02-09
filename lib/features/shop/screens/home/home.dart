1ce41a8ce8de4494ddf4c53901706194c8f9c4d6
LINDEX171 <lindex171@gmail.com>
2025-02-09 16:26:20 +0000

    circular container

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +6,51 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  TCircularContainer(backgroundColor: TColors.textWhite.withOpacity(0.1)),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TCircularContainer extends StatelessWidget {
+  const TCircularContainer({
+    super.key, this.width=400, this.height=400,  this.radius=400,this.padding=400, this.child,this.backgroundColor=TColors.white,
+  });
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final double padding;
+  final Widget? child;
+  final Color backgroundColor;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      padding:  EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        color: backgroundColor,
+      ),
+      child: child,
+    );
   }
 }
