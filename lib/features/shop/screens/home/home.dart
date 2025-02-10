da8c71f85a3fd45da7d1a8aeaf9e149f3f936eb3
LINDEX171 <lindex171@gmail.com>
2025-02-10 13:57:52 +0000

    search bar done

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,10 @@
 import 'package:ecommerce/common/widgets/custom_shapes/containers/primary_headers_container.dart';
+import 'package:ecommerce/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:ecommerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+
+
 
 
 class HomeScreen extends StatelessWidget {
@@ -8,14 +12,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return const  Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
+            ///HEADER
+
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar()
+                  /// APP-BAR
+                  THomeAppBar(),
+
+                   SizedBox(height: TSizes.spaceBtwSections,),
+                  /// Search bar
+                  TSearchContainer(text: "Search in store",)
                 ],
               ),
             )
@@ -25,5 +36,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
 
 
