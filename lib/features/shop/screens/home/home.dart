c56c72b8c0b278404c6155e97db798215605aef5
LINDEX171 <lindex171@gmail.com>
2025-02-10 16:52:59 +0000

    headingtextwidget,categorie images text done

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,18 +1,21 @@
 import 'package:ecommerce/common/widgets/custom_shapes/containers/primary_headers_container.dart';
 import 'package:ecommerce/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:ecommerce/common/widgets/image_text_widgets/vertical_image_text.dart';
+import 'package:ecommerce/common/widgets/text/section_heading.dart';
 import 'package:ecommerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:ecommerce/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
-
-
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -24,9 +27,36 @@ class HomeScreen extends StatelessWidget {
                   /// APP-BAR
                   THomeAppBar(),
 
-                   SizedBox(height: TSizes.spaceBtwSections,),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
                   /// Search bar
-                  TSearchContainer(text: "Search in store",)
+                  TSearchContainer(
+                    text: "Search in store",
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
+                  /// Categories
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(children: [
+                      ///heading
+                      TSectionHeading(
+                        title: 'Popular Categories',
+                        showActionButton: false,
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: TSizes.spaceBtwItems,
+                      ),
+
+                      ///categories
+                      THomeCategories()
+                    ]),
+                  )
                 ],
               ),
             )
