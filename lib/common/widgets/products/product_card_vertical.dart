fec90f9742690baf66c8d97c090a0a43058f212c
LINDEX171 <lindex171@gmail.com>
2025-02-12 16:21:30 +0000

    productcardvertical done

--- lib/common/widgets/products/product_card_vertical.dart
+++ lib/common/widgets/products/product_card_vertical.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce/common/images/t_roundered_image.dart';
 import 'package:ecommerce/common/styles/shadows.dart';
 import 'package:ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:ecommerce/common/widgets/icons/t_circular_icon.dart';
+import 'package:ecommerce/common/widgets/text/product_title_text.dart';
 import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:ecommerce/utils/constants/image_strings.dart';
 import 'package:ecommerce/utils/constants/sizes.dart';
@@ -17,58 +18,124 @@ class TProductCardVertical extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
 
     ///Container with side,paddings,color,edges,radius and shadow
-    return Container(
-      width: 180,
-      padding: const EdgeInsets.all(1),
-      decoration: BoxDecoration(
-          boxShadow: [TShadowStyle.verticalProductShadow],
-          borderRadius: BorderRadius.circular(TSizes.productImageRadius),
-          color: dark ? TColors.darkGrey : TColors.white),
-      child: Column(
-        children: [
-          ///Thumbnails, wishlist Button , discount tag
-          TRoundedContainer(
-            height: 180,
-            padding: const EdgeInsets.all(TSizes.sm),
-            backgroundColor: dark ? TColors.dark : TColors.light,
-            child: Stack(
-              children: [
-                ///Thumbnail image
-                const TRoundedImage(
-                  imageUrl: TImages.productImage1,
-                  applyImageRadius: true,
-                ),
+    return GestureDetector(
+      onTap: () {
 
-                ///sale tag
-                Positioned(
-                  top: 12,
-                  child: TRoundedContainer(
-                    radius: TSizes.sm,
-                    backgroundColor: TColors.secondary.withOpacity(0.8),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: TSizes.sm, vertical: TSizes.xs),
-                    child: Text("25%",
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelLarge!
-                            .apply(color: TColors.black)),
+      },
+      child: Container(
+        width: 180,
+        padding: const EdgeInsets.all(1),
+        decoration: BoxDecoration(
+            boxShadow: [TShadowStyle.verticalProductShadow],
+            borderRadius: BorderRadius.circular(TSizes.productImageRadius),
+            color: dark ? TColors.darkGrey : TColors.white),
+        child: Column(
+          children: [
+            ///Thumbnails, wishlist Button , discount tag
+            TRoundedContainer(
+              height: 180,
+              padding: const EdgeInsets.all(TSizes.sm),
+              backgroundColor: dark ? TColors.dark : TColors.light,
+              child: Stack(
+                children: [
+                  ///Thumbnail image
+                  const TRoundedImage(
+                    imageUrl: TImages.productImage1,
+                    applyImageRadius: true,
                   ),
-                ),
 
-                /// favourite Icon button
-                const Positioned(
-                    top: 0,
-                    right: 0,
-                    child: TCircularIcon(
-                      icon: Iconsax.heart5,
-                      color: Colors.red,
-                    ))
-              ],
+                  ///sale tag
+                  Positioned(
+                    top: 12,
+                    child: TRoundedContainer(
+                      radius: TSizes.sm,
+                      backgroundColor: TColors.secondary.withOpacity(0.8),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: TSizes.sm, vertical: TSizes.xs),
+                      child: Text("25%",
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge!
+                              .apply(color: TColors.black)),
+                    ),
+                  ),
+
+                  /// favourite Icon button
+                  const Positioned(
+                      top: 0,
+                      right: 0,
+                      child: TCircularIcon(
+                        icon: Iconsax.heart5,
+                        color: Colors.red,
+                      ))
+                ],
+              ),
             ),
-          )
+            const SizedBox(
+              height: TSizes.spaceBtwItems / 2,
+            ),
 
-          ///Details
-        ],
+            ///Details
+            Padding(
+              padding: const EdgeInsets.only(left: TSizes.sm),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TProductTitleText(
+                      title: "Green Nike air shoes", smallSize: true),
+                  const SizedBox(
+                    height: TSizes.spaceBtwItems / 2,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "Nike",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.labelMedium,
+                      ),
+                      const SizedBox(
+                        height: TSizes.xs,
+                      ),
+                      const Icon(
+                        Iconsax.verify5,
+                        color: TColors.primary,
+                        size: TSizes.iconXs,
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("\$35.5",
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.headlineMedium),
+                      Container(
+                        decoration: const BoxDecoration(
+                          color: TColors.dark,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(TSizes.cardRadiusMd),
+                              bottomRight:
+                                  Radius.circular(TSizes.productImageRadius)),
+                        ),
+                        child: const SizedBox(
+                            height: TSizes.iconLg * 1.2,
+                            width: TSizes.iconLg * 1.2,
+                            child: Center(
+                              child: Icon(
+                                Iconsax.add,
+                                color: TColors.white,
+                              ),
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
